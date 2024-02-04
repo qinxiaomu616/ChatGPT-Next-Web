85fc88bb68d09f9ac5c56454ae67f76b495ca56f
qinxiaomu616 <87631545+qinxiaomu616@users.noreply.github.com>
2024-02-04 15:40:32 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "中汽传媒智能副驾",
+  description: "你的智能AI助理",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "中汽传媒智能副驾",
     statusBarStyle: "default",
   },
 };
